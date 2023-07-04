7856bd0cade78c67abb5c4cfd59849972bde9c41
Mateus Maciel Nere <mateus.macieln@gmail.com>
2023-07-04 19:11:48 -0300

    Add fetch properties on the relationships fields and use @query to make a custom search with a native query. Also, showing how to use @EntityGraph and @Modifying.

--- src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -1,11 +1,27 @@
 package com.ead.course.repositories;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ead.course.models.ModuleModel;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+
+    /*
+     * @EntityGraph - anotação utilizada para quando for feito o acionamento desse método, o atributo course passará de LAZY para EAGER
+     */
+    // @EntityGraph(attributePaths = {"course"})
+    // ModuleModel findByTitle(String title);
+
+
+    /*
+     * Para utilizar queries nativas para insert, update e delete, utilizar a anotação @Modifying antes de @Query
+     */
+    @Query(value = "select * from tb_modules where course_course_id = :courseId", nativeQuery = true)
+    List<ModuleModel> findAllModulesIntoCourse(@Param("courseId") UUID courseId);
     
 }
