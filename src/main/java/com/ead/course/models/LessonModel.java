e000303e2c84a269453677c9a9b79b6900906cd2
Mateus Maciel Nere <mateus.macieln@gmail.com>
2023-07-03 21:50:12 -0300

    Creating relationships among the models created.

--- src/main/java/com/ead/course/models/LessonModel.java
+++ src/main/java/com/ead/course/models/LessonModel.java
@@ -9,10 +9,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
@@ -40,5 +42,9 @@ public class LessonModel implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     @Column(nullable = false)
     private LocalDateTime creationDate;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne(optional = false)
+    private ModuleModel module;
 
 }
